3bfee031ba1960dda9d599dba99ef8e07e322554
arunkbharathan <arunkbharathan@gmail.com>
2016-12-30 13:01:39 +0530

    dnode stream test

--- dnode/client.js
+++ dnode/client.js
@@ -1,12 +1,27 @@
-const dnode = require('dnode');
+// const dnode = require('dnode');
 
-const d = dnode.connect(20000);
+// const d = dnode.connect(20000);
 
-d.on('remote',  remote => {
-	debugger;
-    remote.transform('beep',  s => {
-    	debugger;
+// d.on('remote',  remote => {
+// 	debugger;
+//     remote.transform('beep',  s => {
+//     	debugger;
+//         console.log('beep => ' + s);
+//         d.end();
+//     });
+// });
+
+
+var dnode = require('dnode');
+var net = require('net');
+
+var d = dnode();
+d.on('remote', function (remote) {
+    remote.transform('beep', function (s) {
         console.log('beep => ' + s);
         d.end();
     });
 });
+
+var c = net.connect(5004);
+c.pipe(d).pipe(c);
--- dnode/server.js
+++ dnode/server.js
@@ -1,9 +1,24 @@
-const dnode = require('dnode');
-const server = dnode({
-	transform : function (s, cb) {
-		console.log('IN');
-		cb(s.replace(/[aeiou]{2,}/, 'oo').toUpperCase());
-	}
+// const dnode = require('dnode');
+// const server = dnode({
+// 	transform : function (s, cb) {
+// 		console.log('IN');
+// 		cb(s.replace(/[aeiou]{2,}/, 'oo').toUpperCase());
+// 	}
+// });
+
+// server.listen(20000);
+
+
+var dnode = require('dnode');
+var net = require('net');
+
+var server = net.createServer(function (c) {
+    var d = dnode({
+        transform : function (s, cb) {
+            cb(s.replace(/[aeiou]{2,}/, 'oo').toUpperCase())
+        }
+    });
+    c.pipe(d).pipe(c);
 });
 
-server.listen(20000);
+server.listen(5004);
